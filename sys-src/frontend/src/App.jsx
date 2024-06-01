bd145f028ed84f757e50761910c6d93b205949a5
Bernhard Gailer <bernhard.gailer@gmx.de>
2024-06-01 15:07:23 +0200

    Add simple example for fetching

--- sys-src/frontend/src/App.jsx
+++ sys-src/frontend/src/App.jsx
@@ -1,15 +1,23 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import GameSelect from './components/GameSelect';
 
 function App() {
-  
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => { console.log(data); return res.json()})
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <>
       <GameSelect></GameSelect>
-
+      <p>{!data ? "Loading..." : data}</p>
 
     </>
   )
