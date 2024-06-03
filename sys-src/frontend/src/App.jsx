8149ac906b419d39c1ae4ab1ce52d989736bd5ba
Bernhard Gailer <bernhard.gailer@gmx.de>
2024-06-03 21:07:30 +0200

    include the new component in the app

--- sys-src/frontend/src/App.jsx
+++ sys-src/frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import { MdSportsSoccer } from "react-icons/md";
 import GameSelect from './components/GameSelect';
 import AppNavbar from './components/AppNavBar';
+import GamePrediction from './components/GamePrediction';
 
 function App() {
 
@@ -35,7 +36,7 @@ function App() {
   return (
     <div id="app">
 
-      <AppNavbar/>
+      <AppNavbar />
 
       <div className="header my-5 d-flex justify-content-center align-items-center flex-column">
         <h1 className="display-1 fw-bold">G<MdSportsSoccer className="text-primary" />al<i className="text-primary">Guru</i></h1>
@@ -44,41 +45,47 @@ function App() {
 
       <div className="my-2 mx-5">
 
-      {/* Game Selection
+        {/* Game Selection
       - We pass down the state changer function and the selected teams as props
       - The state changer function as prop is called from within this component to update the state on the app level here
       */}
-      <GameSelect onTeamSelection={handleTeamSelection} />
-      {/*<p>{!data ? "Loading..." : data}</p>*/}
+        <GameSelect onTeamSelection={handleTeamSelection} />
+        {/*<p>{!data ? "Loading..." : data}</p>*/}
 
 
-      {selectedTeams.every(team => team !== "") ? (
-        <div>
-          <h3 className="text-center m-5">{selectedTeams.join('  vs  ')}</h3>
-          {/* Game Prediction Result*/}
-          {/*<GamePrediction teams={selectedTeams}></GamePrediction>*/}
+        {selectedTeams.every(team => team !== "") ? (
+          <div>
+            <div>
+              <div className="display-5 m-4 mt-5 mx-4 d-flex justify-content-between align-items-center">
+                <p>{selectedTeams[0]}</p>
+                <p>vs.</p>
+                <p>{selectedTeams[1]}</p>
+              </div>
+              {/* Game Prediction Result*/}
+              <GamePrediction teams={selectedTeams}></GamePrediction>
+            </div>
 
-          {/* MatchInfo (previous matches of the two teams)*/}
-          {/*<MatchInfo teams={selectedTeams}></MatchInfo>*/}
+            {/* MatchInfo (previous matches of the two teams)*/}
+            {/*<MatchInfo teams={selectedTeams}></MatchInfo>*/}
 
-          {/* FormInfo Team 1*/}
-          {/*<FormInfo teams={selectedTeams[0]}></FormInfo>*/}
+            {/* FormInfo Team 1*/}
+            {/*<FormInfo teams={selectedTeams[0]}></FormInfo>*/}
 
-          {/* FormInfo Team 2*/}
-          {/*<FormInfo team={selectedTeams[1]}></FormInfo>*/}
+            {/* FormInfo Team 2*/}
+            {/*<FormInfo team={selectedTeams[1]}></FormInfo>*/}
 
-          {/* FormationInfo Team 1*/}
-          {/*<FormationInfo team={selectedTeams[0]}></FormationInfo>*/}
+            {/* FormationInfo Team 1*/}
+            {/*<FormationInfo team={selectedTeams[0]}></FormationInfo>*/}
 
-          {/* FormationInfo Team 2*/}
-          {/*<FormationInfo team={selectedTeams[1]}></FormationInfo>*/}
+            {/* FormationInfo Team 2*/}
+            {/*<FormationInfo team={selectedTeams[1]}></FormationInfo>*/}
 
-        </div>
-      ) : (
-        <h3 className="text-center m-5 text-secondary">Make your first prediction &#9917;</h3>
-      )}
+          </div>
+        ) : (
+          <h3 className="text-center m-5 text-secondary">Make your first prediction &#9917;</h3>
+        )}
 
-    </div>
+      </div>
     </div>
   )
 }
