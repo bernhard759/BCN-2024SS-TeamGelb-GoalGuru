87418150a809dff0140aed49bdda3a3c954e700d
Bernhard Gailer <bernhard.gailer@gmx.de>
2024-06-12 15:12:55 +0200

    add axios and testing scripts

--- sys-src/frontend/vite.config.js
+++ sys-src/frontend/vite.config.js
@@ -4,6 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/__tests__/setup.js',
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      reporter: ['text', 'html'], // Add 'html' reporter for UI coverage
+    },
+  },
   server: {
     proxy: {
       '/api': {
