7a5d705ac3a6e266443b671ed82accad8ab5718e
Maria Lyoteva <maria.lyoteva@gmail.com>
2024-06-12 16:13:54 +0200

    reversion

--- sys-src/backend/server.py
+++ sys-src/backend/server.py
@@ -6,35 +6,6 @@ import logging
 import requests
 
 app = FastAPI()
-#Dummy data
-teams = ["Team A", "Team B", "Team C", "Team D"]
-matches = [
-    {"home_team": "Team A", "away_team": "Team B", "home_goals": 3, "away_goals": 1, "date": "2023-04-15"},
-    {"home_team": "Team A", "away_team": "Team C", "home_goals": 1, "away_goals": 1, "date": "2023-04-16"},
-]
-
-
-#Define response models
-class TeamsResponse(BaseModel):
-    teams: List[str]
-
-class MatchesResponse(BaseModel):
-    home_team: str
-    away_team: str
-    home_goals: int
-    away_goals: int
-    date: str
-
-# Endpoint for available teams
-@app.get("/api/teams", response_model=TeamsResponse)
-async def get_teams():
-    logging.info("Received request for teams")
-    return {"teams": teams}
-
-@app.get("/api/matches",  response_model=List[MatchesResponse])
-async def get_matches():
-     logging.info("Received request for matches")
-     return matches
 
 
 @app.get("/api")
