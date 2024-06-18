bf19fb4672769fd0c204d30ca9a3622b18f4e46a
Tsvetan Stanchev <t.stanchev@oth-aw.de>
2024-06-18 14:55:50 +0200

    Methods in utils.py

--- sys-src/backend/utils.py
+++ sys-src/backend/utils.py
@@ -1,7 +1,24 @@
+import re
 import requests
 import difflib
 import pandas as pd
+from bs4 import BeautifulSoup
 import models
+
+bundesliga_1 = {
+    "Bayer": 15, "Bayern": 27, "Stuttgart": 79, "Leipzig": 23826,
+    "Borussia": 18, "Eintracht": 24, "Freiburg": 60, "Augsburg": 167,
+    "Hoffenheim": 533, "Heidenheim": 2036, "Werder": 86, "Wolfsburg": 82,
+    "Union": 89, "Bochum": 80, "Mainz": 39, "Köln": 3, "Darmstadt": 105
+}
+
+header = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/123.0.0.0 Safari/537.36"
+    )
+}
 
 """
 Methods used in our ML models for receiving necessary input data and for our Fast-API Server
@@ -45,14 +62,80 @@ def get_market_value(team):
 
 
 #Returns the market value of a given team from webscraping (transfermarkt)
-def get_market_value_from_web(team):
-    pass
+def get_market_value_web(team):
+    team_id = bundesliga_1.get(team)
+    if not team_id:
+        print(f"Team {team} not found in the dictionary.")
+        return None
+
+    url = f"https://www.transfermarkt.com/{team}/startseite/verein/{team_id}"
+
+    try:
+        response = requests.get(url, headers=header)
+        response.raise_for_status()  # Raise an exception for HTTP errors
+
+        # Use a regular expression to find the market value in the HTML content
+        match = re.search(
+            r'<a href="[^"]+" class="data-header__market-value-wrapper">\s*<span class="waehrung">€</span>([\d.,]+)\s*<span class="waehrung">m</span>',
+            response.text)
+
+        if match:
+            market_value = match.group(1).replace(',', '')
+            return market_value
+        else:
+            print("Market value not found")
+
+    except Exception as err:
+        print(f"An error occurred: {err}")
+
+    return None
 
 
 #Return last n_matches of two teams (transfermarkt)
 #[home, away, goal_home, goal_away, date]
-def get_last_matches(team_one, team_two, n_matches):
-    pass
+def get_last_matches_web(team_a, team_b,  n):
+    base_url = "https://www.transfermarkt.de/vergleich/bilanzdetail/verein/{}/gegner_id/{}"
+
+    try:
+        a = bundesliga_1[team_a]
+        b = bundesliga_1[team_b]
+
+        this_url = base_url.format(a, b)
+
+        response = requests.get(this_url, headers=header)
+        response.raise_for_status()  # Raise an exception for bad status codes
+        soup = BeautifulSoup(response.content, "html.parser")
+    except requests.exceptions.RequestException as e:
+        print("Error fetching data:", e)
+
+        # getting the table with the results between the teams
+    table = soup.select_one('table[class="items"]')
+    tr_elements = table.find_all('tr')
+    data = []
+
+    for tr in tr_elements:
+        td_elements = tr.find_all('td')
+        td_row = []
+        for td in td_elements:
+            td_row.append(td.text.strip())
+        data.append(td_row)
+
+    data = [[elem for elem in row if elem.strip()] for row in data]
+    data = data[1:]
+    data = [row for row in data if len(row) >= 5]
+    data = data[:n]
+
+    winners = []
+    for row in data:
+        result = row[7].split(':')
+        if int(result[0]) > int(result[1]):
+            winners.append(team_a)
+        elif int(result[0]) < int(result[1]):
+            winners.append(team_b)
+        else:
+            winners.append("Draw")
+
+    return winners
 
 
 #list all teams of the first bl from csv
@@ -81,3 +164,6 @@ def get_all_teams_from_web(season = 2023):
 def predict(home, away):
     model = models.ModelOne()
     return model.predict()
+
+
+print(get_last_matches_web("Bayern", "Bayer", 10))
