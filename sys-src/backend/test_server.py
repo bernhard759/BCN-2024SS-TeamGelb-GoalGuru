7d20efdb9bee8908197c013cc1458bb0abd7a268
Maria Lyoteva <maria.lyoteva@gmail.com>
2024-06-22 20:24:07 +0200

    test for get_matches with dummy data

--- sys-src/backend/test_server.py
+++ sys-src/backend/test_server.py
@@ -8,5 +8,15 @@ async def test_get_teams():
         response = await ac.get("/api/teams")
     assert response.status_code == 200
     assert response.json() == {"teams": ["Team A", "Team B", "Team C", "Team D"]}
+    
+@pytest.mark.asyncio
+async def test_get_matches():
+    async with AsyncClient(app=app, base_url="http://test") as ac:
+        response = await ac.get("/api/matches")
+    assert response.status_code == 200
+    assert response.json() == [
+        {"home_team": "Team A", "away_team": "Team B", "home_goals": 3, "away_goals": 1, "date": "2023-04-15"},
+        {"home_team": "Team A", "away_team": "Team C", "home_goals": 1, "away_goals": 1, "date": "2023-04-16"},
+    ]
 
 
