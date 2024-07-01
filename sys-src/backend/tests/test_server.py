946fabd1b6894ad6e37cdde6974a58def11dd6b2
Bernhard Gailer <bernhard.gailer@gmx.de>
2024-07-01 15:24:13 +0200

    change tests because now we have sync_team_names

--- sys-src/backend/tests/test_server.py
+++ sys-src/backend/tests/test_server.py
@@ -27,11 +27,11 @@ async def test_get_matches():
     assert response.status_code == 200
     
 @pytest.mark.asyncio
-async def test_predict_fail():
-    home_team = "Team A"
-    away_team = "Team B"
+async def test_predict():
+    home_team = "Union Berlin"
+    away_team = "RasenBallsport Leipzig"
     async with AsyncClient(app=app, base_url="http://test") as ac:
         response = await ac.get(f"/api/predict?home_team={home_team}&away_team={away_team}")
-    assert response.status_code == 500
+    assert response.status_code == 200
     
 
